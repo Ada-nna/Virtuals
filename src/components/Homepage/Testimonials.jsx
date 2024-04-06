b4ff9220bfefe4845fa87beff0bcd609f0e87205
Ada-nna <cyndigal2@gmail.com>
2024-04-07 00:14:46 +0100

    new update

--- src/components/Homepage/Testimonials.jsx
+++ src/components/Homepage/Testimonials.jsx
@@ -28,7 +28,7 @@ export const TestimonialCards = ({ text, image, name, organization }) => {
 const Testimonials = () => {
   return (
     <div className="mt-[5rem] lg:mt-[6rem] max-w-7xl mx-auto md:px-0 px-4 border-b border-neutral-800 pb-[7rem]">
-      <h1 className="text-[35px] lg:text-6xl text-center">
+      <h1 className="text-[35px] lg:text-6xl text-center leading-[46px] lg:leading-none">
         What People{" "}
         <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
           Are Saying
